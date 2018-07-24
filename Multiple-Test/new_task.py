e907423d41cac7382596869c58948a2ddf676c89
Anway Somani <=>
2018-07-24 10:41:55 +0530

    Changes to locals

--- Multiple-Test/new_task.py
+++ Multiple-Test/new_task.py
@@ -1,13 +1,18 @@
 import pika
 import sys
 
-connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
 channel = connection.channel()
 
-message = ''.join(sys.argv[1:])
-channel.basic_publish(exchange='',
-                      routing_key='hello',
-                      body=message)
-print(" [x] Sent %r" % message)
+channel.queue_declare(queue='task_queue', durable=True)
 
+message = ' '.join(sys.argv[1:]) or "Hello World!"
+channel.basic_publish(exchange='',
+                      routing_key='task_queue',
+                      body=message,
+                      properties=pika.BasicProperties(
+                          delivery_mode = 2,
+                      ))
+
+print(" [x] Sent %r" % message)
 connection.close()
